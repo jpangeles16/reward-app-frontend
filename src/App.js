fb5babeb35088fb1b0a542e46c857b38b7f24e13
John <jpangeles16@gmail.com>
2021-02-05 17:23:59 -0800

    Added confetti and updated code to allow for api call to flask backend

--- src/App.js
+++ src/App.js
@@ -1,20 +1,20 @@
 import { ReportSet } from './components/ReportSet';
 import './styles/app.css';
 
-const reports = [
-  {
-    'date': '08/07',
-    'points': 5
-  },
-  {
-    'date': '08/08',
-    'points': 6
-  },
-  {
-    'date': '08/09',
-    'points': 7
-  }
-];
+// const reports = [
+//   {
+//     'date': '08/07',
+//     'points': 5
+//   },
+//   {
+//     'date': '08/08',
+//     'points': 6
+//   },
+//   {
+//     'date': '08/09',
+//     'points': 7
+//   }
+// ];
 
 function App() {
 
@@ -25,13 +25,13 @@ function App() {
       xmlHttp.send( null );
       return xmlHttp.responseText;
   }
-
-  console.log(httpGet("http://127.0.0.1:5000/api/John"));
-  console.log('test');
-
+  let report = JSON.parse(httpGet("http://127.0.0.1:5000/api/John"));
   return (
+    // <div className="App">
+    //   <ReportSet tutee="John" reports={reports} />
+    // </div>
     <div className="App">
-      <ReportSet tutee="John" reports={reports} />
+      <ReportSet tutee={report.name} points={report.points} />
     </div>
   );
 }
--- src/components/ReportSet.js
+++ src/components/ReportSet.js
@@ -4,35 +4,69 @@ import { Accordion } from './Accordion';
 import { ProgressBar} from './ProgressBar';
 import '../styles/report-set.css';
 
+const RANKS = [
+  'an apprentice',
+  'a beginner',
+  'a learner',
+  'an associate learner',
+  'an advanced learner',
+  'a master learner',
+  'an expert learner',
+  'an out of this world learner.  You broke the scale'
+]
+
 export function ReportSet(props) {
 
-  const totalPoints = props.reports.map((report) =>
-   report.points).reduce((accum, score) =>
-    accum + score);
+  // const totalPoints = props.reports.map((report) =>
+  //  report.points).reduce((accum, score) =>
+  //   accum + score);
 
-  const accordions = props.reports.map((report, index) => 
-    <Accordion key={index}
-      title={`Report on ${report.date}`}
-      studentScore={report.points} 
-      content={"Test"} />
-  );
+  // const accordions = props.reports.map((report, index) => 
+  //   <Accordion key={index}
+  //     title={`Report on ${report.date}`}
+  //     studentScore={report.points} 
+  //     content={"Test"} />
+  // );
+
+  function determineRank(points) {
+    if (points <= 10)
+      return RANKS[0];
+    else if (points > 10 && points <= 20)
+      return RANKS[1];
+    else if (points > 20 && points <= 30)
+      return RANKS[2];
+    else if (points > 30 && points <= 40)
+      return RANKS[3];
+    else if (points > 40 && points <= 50)
+      return RANKS[4];
+    else if (points > 50 && points <= 60)
+      return RANKS[5];
+    else if (points > 60 && points <= 70)
+      return RANKS[6];
+    else
+      return RANKS[7];
+  }
 
   return (
       <section>
         <header>
           <h1>Hello, {props.tutee}!</h1>
         </header>
-        <div className="accordion-set">
+        {/* <div className="accordion-set">
           {accordions}
+        </div> */}
+        <div className="report-data">
+          <p className="total-points-paragraph">Your total points are {props.points}!
+            <br />          
+           You are currently {determineRank(props.points)}!</p>
+          <ProgressBar totalPoints={props.points} />
+          <p className="total-points-paragraph">Keep up the good work, {props.tutee}!</p>
         </div>
-        <p className="total-points-paragraph">Your total points are {totalPoints}!</p>
-        <ProgressBar totalPoints={totalPoints} />
-        <p className="total-points-paragraph">Keep up the good work, {props.tutee}!</p>
       </section>
     );
 }
 
 ReportSet.propTypes = {
   tutee: PropTypes.string,
-  reports: PropTypes.array
+  points: PropTypes.number
 }
