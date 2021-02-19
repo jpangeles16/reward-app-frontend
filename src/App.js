e27a315fb87266d1f7667748e5de4e0a3e1f9638
John <jpangeles16@gmail.com>
2021-02-18 16:57:17 -0800

    Switching up the colors

--- src/App.js
+++ src/App.js
@@ -1,21 +1,6 @@
 import { ReportSet } from './components/ReportSet';
 import './styles/app.css';
 
-// const reports = [
-//   {
-//     'date': '08/07',
-//     'points': 5
-//   },
-//   {
-//     'date': '08/08',
-//     'points': 6
-//   },
-//   {
-//     'date': '08/09',
-//     'points': 7
-//   }
-// ];
-
 function App() {
 
   function httpGet(theUrl)
@@ -26,12 +11,10 @@ function App() {
       return xmlHttp.responseText;
   }
   let report = JSON.parse(httpGet("http://127.0.0.1:5000/api/John"));
+  console.log(report)
   return (
-    // <div className="App">
-    //   <ReportSet tutee="John" reports={reports} />
-    // </div>
     <div className="App">
-      <ReportSet tutee={report.name} points={report.points} />
+      <ReportSet tutee={report.name} points={60} />
     </div>
   );
 }
--- src/components/ProgressBar.js
+++ src/components/ProgressBar.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import '../styles/progress-bar.css';
 
-export function ProgressBar(props) {
-    return (
-        <div className="progress-bar" 
-            data-label=""
-            style={{"--width": props.totalPoints}}>
+export function ProgressBar({totalPoints}) {
+    const [style, setStyle] = React.useState({});
+    setTimeout(() => {
+        const newStyle = {
+            opacity: 1,
+            width: `${totalPoints}%`
+        }
+        setStyle(newStyle);
 
+    }, 2000);
+    return (
+        <div className="progress">
+            <div className="progress-done" style={style}>
+                {totalPoints}
+            </div>
         </div>
     )
 }
